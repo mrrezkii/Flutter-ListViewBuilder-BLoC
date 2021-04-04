f100ade85e4e585de3656c21fe4c6e3e8b725697
nando <muhammad.rezki.ananda@gmail.com>
2021-04-05 00:45:22 +0700

    ADD | ListViewBuilder + BLoC

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
+import 'package:bloc/bloc.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:listviewbuilder_bloc/product_card.dart';
-import 'package:provider/provider.dart';
 
 void main() {
   runApp(MyApp());
@@ -11,48 +14,92 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        appBar: AppBar(
-          backgroundColor: firstColor,
-        ),
-        body: ChangeNotifierProvider<ProductState>(
-          create: (context) => ProductState(),
-          child: Container(
-            margin: EdgeInsets.all(20),
-            child: Align(
-              alignment: Alignment.topCenter,
-              child: Consumer<ProductState>(
-                builder: (context, productState, _) => ProductCard(
-                  imageUrl:
-                      "https://cdn.myanimelist.net/images/characters/2/266011.jpg",
-                  name: "Filo chan imoetz",
-                  price: "Rp1.000.00",
-                  quantity: productState.quantity,
-                  onAddCartTap: () {},
-                  onIncTap: () {
-                    productState.quantity++;
-                  },
-                  onDecTap: () {
-                    productState.quantity--;
-                  },
-                  notification:
-                      (productState.quantity > 5) ? "Diskon 10 %" : null,
-                ),
+      home:
+          BlocProvider(create: (context) => ProductBloc([]), child: MainPage()),
+    );
+  }
+}
+
+class MainPage extends StatelessWidget {
+  Random random = Random();
+  @override
+  Widget build(BuildContext context) {
+    ProductBloc bloc = BlocProvider.of<ProductBloc>(context);
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Color(0xffF44336),
+        title: Text("Demo ListView Builder"),
+      ),
+      body: Column(
+        children: <Widget>[
+          RaisedButton(
+              color: Color(0xffF44336),
+              child: Text(
+                "Create Product",
+                style: TextStyle(color: Colors.white),
+              ),
+              onPressed: () {
+                bloc.add(random.nextInt(4) + 2);
+              }),
+          SizedBox(height: 20),
+          BlocBuilder<ProductBloc, List<Product>>(
+            builder: (context, products) => Expanded(
+              child: ListView.builder(
+                scrollDirection: Axis.horizontal,
+                itemCount: products.length,
+                itemBuilder: (context, index) {
+                  return Row(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      (index == 0)
+                          ? SizedBox(
+                              width: 20,
+                            )
+                          : Container(),
+                      ProductCard(
+                        imageUrl: products[index].imageURL,
+                        name: products[index].name,
+                        price: products[index].price.toString(),
+                        onAddCartTap: () {},
+                        onIncTap: () {},
+                        onDecTap: () {},
+                      ),
+                      SizedBox(
+                        width: 20,
+                      )
+                    ],
+                  );
+                },
               ),
             ),
-          ),
-        ),
+          )
+        ],
       ),
     );
   }
 }
 
-class ProductState with ChangeNotifier {
-  int _quantity = 0;
+class Product {
+  String imageURL;
+  String name;
+  int price;
 
-  int get quantity => _quantity;
-  set quantity(int newValue) {
-    _quantity = newValue;
-    notifyListeners();
+  Product({this.imageURL = "", this.name = "", this.price = 0});
+}
+
+class ProductBloc extends Bloc<int, List<Product>> {
+  ProductBloc(List<Product> initialState) : super(initialState);
+
+  @override
+  Stream<List<Product>> mapEventToState(int event) async* {
+    List<Product> products = [];
+    for (int i = 0; i < event; i++) {
+      products.add(Product(
+          imageURL:
+              "https://cdna.artstation.com/p/assets/images/images/018/441/380/large/arden-galdones-brianna-filo-commission-low-res.jpg",
+          name: "Produk " + i.toString(),
+          price: (i + 1) * 5000));
+    }
+    yield products;
   }
 }
